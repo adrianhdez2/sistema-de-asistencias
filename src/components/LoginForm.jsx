c25b2b90993a3dd0a5491e6887e561d64d00fd5e
adrianhdez2 <cesarahh3@gmail.com>
2024-06-13 15:32:27 -0600

    some changes

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,14 @@
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+import UsePortals from "./hooks/usePortals"
+import Error from "./items/Error"
+import Message from "./items/Message"
 
 
 function LoginForm({ handleHiddeLoginForm }) {
+    const [error, setError] = useState(null)
+    const [message, setMessage] = useState(null)
     const navigate = useNavigate()
     const [values, setValues] = useState({
         correo: '',
@@ -28,80 +33,114 @@ function LoginForm({ handleHiddeLoginForm }) {
 
         axios.post('http://localhost:3001/admins/login', values)
             .then(res => {
-                if (res.data.status){
+                if (res.data.status) {
                     navigate('/dashboard')
                 }
             })
-            .catch(err => console.log(err))
+            .catch(err => setError(err.response?.data?.error))
     }
 
-    return (
-        <div className="absolute top-0 left-0 bg-gray-500/50 w-full h-dvh flex items-center justify-center">
-            <form className='w-1/3 bg-white mx-auto rounded-md p-4' onSubmit={handleSubmit}>
-                <div className="w-full flex items-center justify-center text-center py-2 mb-2">
-                    <h3 className="text-2xl font-semibold">Inicia sesión</h3>
-                </div>
-                <div>
-                    <label
-                        htmlFor="matricula"
-                        className="block text-sm font-medium leading-6 text-gray-900"
-                    >
-                        Correo electrónico:
-                    </label>
-                    <div className="mt-2">
-                        <input
-                            id="correo"
-                            name="correo"
-                            type="email"
-                            autoComplete="off"
-                            required
-                            value={values.correo}
-                            onChange={handleChangeValues}
-                            className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                </div>
+    useEffect(() => {
+        if (error) {
+            setError(error);
+            const timer = setTimeout(() => {
+                setError('');
+            }, 5000);
 
-                <div className="my-4">
-                    <div className="flex items-center justify-between">
+            return () => clearTimeout(timer);
+        }
+    }, [error]);
+
+    useEffect(() => {
+        if (message) {
+            setMessage(message);
+            const timer = setTimeout(() => {
+                setMessage('');
+            }, 5000);
+
+            return () => clearTimeout(timer);
+        }
+    }, [message]);
+
+    return (
+        <>
+            <div className="absolute top-0 left-0 bg-gray-500/50 w-full h-dvh flex items-center justify-center">
+                <form className='w-1/3 bg-white mx-auto rounded-md p-4' onSubmit={handleSubmit}>
+                    <div className="w-full flex items-center justify-center text-center py-2 mb-2">
+                        <h3 className="text-2xl font-semibold">Inicia sesión</h3>
+                    </div>
+                    <div>
                         <label
-                            htmlFor="password"
+                            htmlFor="matricula"
                             className="block text-sm font-medium leading-6 text-gray-900"
                         >
-                            Contraseña:
+                            Correo electrónico:
                         </label>
+                        <div className="mt-2">
+                            <input
+                                id="correo"
+                                name="correo"
+                                type="email"
+                                autoComplete="off"
+                                required
+                                value={values.correo}
+                                onChange={handleChangeValues}
+                                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
                     </div>
-                    <div className="mt-2">
-                        <input
-                            id="password"
-                            name="password"
-                            type="password"
-                            autoComplete="off"
-                            required
-                            value={values.password}
-                            onChange={handleChangeValues}
-                            className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                </div>
 
-                <div className="flex w-full flex-col gap-y-3 mt-5">
-                    <button
-                        type="submit"
-                        className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    >
-                        Entrar
-                    </button>
-                    <button
-                        type="button"
-                        onClick={handleHiddeLoginForm}
-                        className="text-sm font-semibold leading-6 text-gray-900 bg-gray-100 hover:bg-gray-300 px-3 py-2 rounded-md"
-                    >
-                        Cancelar
-                    </button>
-                </div>
-            </form>
-        </div>
+                    <div className="my-4">
+                        <div className="flex items-center justify-between">
+                            <label
+                                htmlFor="password"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                            >
+                                Contraseña:
+                            </label>
+                        </div>
+                        <div className="mt-2">
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="off"
+                                required
+                                value={values.password}
+                                onChange={handleChangeValues}
+                                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                        </div>
+                    </div>
+
+                    <div className="flex w-full flex-col gap-y-3 mt-5">
+                        <button
+                            type="submit"
+                            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            Entrar
+                        </button>
+                        <button
+                            type="button"
+                            onClick={handleHiddeLoginForm}
+                            className="text-sm font-semibold leading-6 text-gray-900 bg-gray-100 hover:bg-gray-300 px-3 py-2 rounded-md"
+                        >
+                            Cancelar
+                        </button>
+                    </div>
+                </form>
+            </div>
+            {error &&
+                <UsePortals>
+                    <Error error={error} />
+                </UsePortals>
+            }
+            {message &&
+                <UsePortals>
+                    <Message message={message} />
+                </UsePortals>
+            }
+        </>
     )
 }
 
